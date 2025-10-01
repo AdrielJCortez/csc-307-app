c9dd9c8e687919a01a5f9fc79bf0407ea907b155
Adriel_Cortez <adrieljcortez@outlook.com>
2025-10-01 16:35:25 -0700

    added find user by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -34,12 +34,25 @@ const users = {
 };
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
 app.get("/users", (req, res) => {
     res.send(users);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
 });
 
 app.listen(port, () => {
